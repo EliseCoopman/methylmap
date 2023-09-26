ec9f3e56dbc96e94c347466f4b1e0e8279cfb67b
EliseCoopman <elisecoopman@yahoo.com>
2023-09-26 15:10:57 +0200

    update dash application

--- methylmap/methylmap.py
+++ methylmap/methylmap.py
@@ -10,6 +10,7 @@ from dash import Dash, dcc, html, Input, Output, State, ctx
 
 import os
 import sys
+import re
 import numpy as np
 from argparse import ArgumentParser
 
@@ -28,16 +29,47 @@ def main():
             html.Button(id="button-o10", n_clicks=0, children="Zoom out 10x"),
             html.Button(id="button-i3", n_clicks=0, children="Zoom in 3x"),
             html.Button(id="button-i10", n_clicks=0, children="Zoom in 10x"),
-            dcc.RadioItems(
-                options=["File order", "Hierarchical clustering"],
-                value="Hierarchical clustering" if args.dendro else "File order",
-                inline=True,
-                id="dendro",
+            html.Div([  # Wrap Annotation and annotation-type in a common div
+                dcc.Checklist(
+                    id="annotation",
+                    options=[{"label": "Annotation", "value": "annotation"}],
+                    value=[],
+                    style={'display': 'inline-block'},
+                ),
+                dcc.RadioItems(
+                    id='annotation-type',
+                    options=[
+                        {'label': 'Gene', 'value': 'gene'},
+                        {'label': 'Transcript', 'value': 'transcript'},
+                    ],
+                    value='gene',
+                    labelStyle={'display': 'inline-block'},
+                    style={'display': 'inline-block'},
+                ),
+            ]),
+            dcc.Checklist(
+                id="hierarchical_clustering",
+                options=[{"label": "Hierarchical clustering", "value": "dendro"}],
+                value=[],
             ),
+            html.Div(id='error-message', style={'color': 'red'}),
             meth_browser(app, args),
         ]
     )
+
+    @app.callback(
+        Output('annotation-type', 'style'),
+        Input('annotation', 'value')
+    )
+    def toggle_annotation_type_visibility(annotation):
+        if 'annotation' in annotation:
+            return {'display': 'block'}  # Show RadioItems when 'Annotation' is checked
+        else:
+            return {'display': 'none'}  # Hide RadioItems when 'Annotation' is unchecked
+
+
     app.run_server(debug=True)
+    # app.run()
 
 
 def get_args():
@@ -115,21 +147,38 @@ def get_args():
 
 def meth_browser(app, args):
     @app.callback(
-        Output(component_id="plot", component_property="children"),
-        [
-            Input(component_id="confirm-button", component_property="n_clicks"),
-            Input(component_id="button-o3", component_property="n_clicks"),
-            Input(component_id="button-o10", component_property="n_clicks"),
-            Input(component_id="button-i3", component_property="n_clicks"),
-            Input(component_id="button-i10", component_property="n_clicks"),
-            Input(component_id="dendro", component_property="value"),
-        ],
-        [State(component_id="input-box", component_property="children")],
-    )
+    [Output(component_id="plot", component_property="children"),
+     Output('error-message', 'children')],
+    [
+        Input(component_id="confirm-button", component_property="n_clicks"),
+        Input(component_id="button-o3", component_property="n_clicks"),
+        Input(component_id="button-o10", component_property="n_clicks"),
+        Input(component_id="button-i3", component_property="n_clicks"),
+        Input(component_id="button-i10", component_property="n_clicks"),
+        Input(component_id="hierarchical_clustering", component_property="value"),
+        Input(component_id="annotation", component_property="value"),
+        Input(component_id="annotation-type", component_property="value"), 
+    ],
+    [State(component_id="input-box", component_property="children")],
+)
     def update_plots(
-        button_confirm, button_o3, button_o10, button_i3, button_i10, switch_dendro, window
+        button_confirm,
+        button_o3,
+        button_o10,
+        button_i3,
+        button_i10,
+        hierarchical_clustering,
+        annotation,
+        annotation_type,
+        window,
     ):
-        window = Region(window["props"]["value"]) if window else Region(args.window)
+        window_input = window["props"]["value"] if window else args.window
+        
+        # Validate the input format
+        if not validate_input(window_input):
+            return None, 'Invalid input format. Please enter genomic region in a valid format. Example chr20:58,839,718-58,911,192 or chr20:58839718-58911192'
+        
+        window = Region(window_input)
         if "button-o3" == ctx.triggered_id:
             window = window * 3
         elif "button-o10" == ctx.triggered_id:
@@ -138,9 +187,19 @@ def meth_browser(app, args):
             window = window / 3
         elif "button-i10" == ctx.triggered_id:
             window = window / 10
-        dendro = True if switch_dendro == "Hierarchical clustering" else False
-        num_col = 2 if args.gff else 1  # number of subplots (columns) needed
+
+        # Convert checklist values to boolean flags
+        dendro = "dendro" in hierarchical_clustering
+        annotation = "annotation" in annotation
+        if annotation:
+            if annotation_type == 'gene':
+                simplify = True
+            if annotation_type == 'transcript':
+                simplify = False
+
         num_row = 2 if dendro else 1
+        num_col = 2 if annotation else 1  # number of subplots (columns) needed
+        
         subplots = plots.create_subplots(num_col, num_row)
         # frequencies table with all meth frequencies of all samples
         meth_data, window = read_mods(
@@ -155,12 +214,12 @@ def meth_browser(app, args):
             dendro,
         )
         if dendro:
-            meth_data, den, list_sorted_samples = dendrogram.make_dendro(meth_data, window)
+            meth_data, den, list_sorted_samples = dendro.make_dendro(meth_data, window)
         meth_data.to_csv(args.outtable, sep="\t", na_rep=np.NaN, header=True)
         fig = plots.plot_methylation(subplots, meth_data, num_col, num_row)
 
-        if args.gff:
-            annotation_traces = annotation.gff_annotation(args.gff, window, args.simplify)
+        if annotation:
+            annotation_traces = annotation.gff_annotation(args.gff, window, simplify)
             for annot_trace in annotation_traces:
                 fig.append_trace(trace=annot_trace, row=num_row, col=1)
             fig.update_xaxes(
@@ -190,14 +249,13 @@ def meth_browser(app, args):
             fig.update_layout(showlegend=False)
             fig["data"][0]["x"] = den.layout.xaxis.tickvals
 
-            dendro_xaxis = "xaxis4" if args.gff else "xaxis2"
+            dendro_xaxis = "xaxis4" if annotation  else "xaxis2"
             fig["layout"][dendro_xaxis]["tickvals"] = den.layout.xaxis.tickvals
             fig["layout"][dendro_xaxis]["ticktext"] = list_sorted_samples
-            # plots.create_output_methylmap(fig, outfig, window)
-        return html.Div(dcc.Graph(figure=fig), id="plot")
+            #plots.create_output_methylmap(fig, outfig, window)
+        return html.Div(dcc.Graph(figure=fig), id="plot"), None  # No error message
 
     return html.Div(id="plot")
-
 
 def input_box(app, args):
     @app.callback(
@@ -220,9 +278,21 @@ def input_box(app, args):
             window = window / 3
         elif "button-i10" == ctx.triggered_id:
             window = window / 10
+
         return html.Div(dcc.Input(type="text", value=window.fmt), id="input-box")
 
     return html.Div(dcc.Input(type="text", value=args.window), id="input-box")
+
+def validate_input(input_text):
+    # Define a regular expression pattern for the valid formats
+    pattern_with_commas = r'^chr\d+:\d+,\d+,\d+-\d+,\d+,\d+$'
+    pattern_without_commas = r'^chr\d+:\d+-\d+$'
+    
+    # Use the re.match() function to check if the input matches either pattern
+    if re.match(pattern_with_commas, input_text) or re.match(pattern_without_commas, input_text):
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
