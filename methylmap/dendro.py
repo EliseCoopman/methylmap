1e20708644728f56c839a3ab0ccba17a4d7332d4
EliseCoopman <elisecoopman@yahoo.com>
2024-12-13 13:53:03 +0100

    change interpolation axis to vertical in make_dendro function

--- methylmap/dendro.py
+++ methylmap/dendro.py
@@ -5,7 +5,6 @@ import plotly.figure_factory as ff
 
 
 def make_dendro(methfreqtable):
-    print(methfreqtable)
     methfreqtable = methfreqtable.apply(pd.to_numeric, errors="coerce")
     number_of_nan_values = methfreqtable.isna().sum().sum()
     if number_of_nan_values != 0:
@@ -15,7 +14,7 @@ def make_dendro(methfreqtable):
         sys.stderr.write(
             f"\n\n{number_of_nan_values} NaN values found in data. NaN values will be estimated using numpy interpolate to perform hierarchical clustering.\n\n"
         )
-        methfreqtable.interpolate(method="linear", axis=1, inplace=True)
+        methfreqtable.interpolate(method="linear", axis=0, inplace=True)
     number_of_nan_values_interpolate = methfreqtable.isna().sum().sum()
     if number_of_nan_values_interpolate != 0:
         logging.warning(
@@ -25,7 +24,6 @@ def make_dendro(methfreqtable):
             f"\n\n{number_of_nan_values_interpolate} NaN values found in data after using numpy interpolate for estimation of these values. Positions with minimal 1 NaN value will be deleted to perform hierarchical clustering.\n\n"
         )
     methfreqtable.dropna(inplace=True)
-
     methfreqtable_transposed = methfreqtable.transpose()
     samples = methfreqtable_transposed.index.tolist()
     methfreqtable_transposed.reset_index(drop=True, inplace=True)
